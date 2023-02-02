5bf5ebc81a8b59dbe88226227f5936b3740201e8
Obediah Klopfenstein <obe711@gmail.com>
2023-02-01 19:51:36 -0600

    added checkboxes to user list

--- src/components/SerialPortOption.tsx
+++ src/components/SerialPortOption.tsx
@@ -2,8 +2,11 @@
 
 function SerialPortOption({ serialPort }: { serialPort: string }) {
 	return (
-		<li>
-			<span>{serialPort}</span>
+		<li className="serial-option">
+			{/* <span className="serial-option">{serialPort}</span> */}
+			<input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+			<label htmlFor="vehicle1">{serialPort}</label>
+			<br></br>
 		</li>
 	);
 }
